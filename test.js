0c716e95010fce1f248727469dd43364b867d7d4
Joey Zhang <appleyboi13@gmail.com>
2025-08-01 16:24:51 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,5 @@
+let b = document.getElementById("myCanvas")
+let can = b.getContext("2d")
+can.fillStyle = "rgb(255, 255, 255)"
+can.fillRect(40, 40, 40, 40)
+
---
+++ test2.js
@@ -0,0 +1,5 @@
+let color = "rgb(174, 174, 174)"
+let clicked = 0;
+if (clicked == 1) {
+    color = "rgb(255, 255, 255)";
+}
